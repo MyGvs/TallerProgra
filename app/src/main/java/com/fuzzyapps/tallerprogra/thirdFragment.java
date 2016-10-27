7673fb60b678ed60e1e64a176f65150f38e3bac0
Alvaro <alvaro.mamani.limachi@gmail.com>
2016-10-27 12:45:28 -0400

    Grand Slam

--- app/src/main/java/com/fuzzyapps/tallerprogra/thirdFragment.java
+++ app/src/main/java/com/fuzzyapps/tallerprogra/thirdFragment.java
@@ -7,13 +7,20 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class thirdFragment extends Fragment {
-
+    // Inputs
+    private EditText year;
+    private Button crear;
+    // Instancia de SQLite
+    private SQLite sqlite;
 
     public thirdFragment() {
         // Required empty public constructor
@@ -28,7 +35,20 @@ public class thirdFragment extends Fragment {
     @Override
     public void onViewCreated(final View view, @Nullable final Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        sqlite= new SQLite(getActivity());
+        sqlite.abrir();
         //AQUI INICIALIZAR LOS OBJETOS
+        year = (EditText) view.findViewById(R.id.year);
+        crear = (Button) view.findViewById(R.id.createGs);
+        crear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(getActivity(), "Torneo creado", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        sqlite.cerrar();
     }
 
 }
