6e45f40808ace95013b60dd3c758c3d051e58573
Fernando <ferna.mace.34@gmail.com>
2016-11-03 19:03:04 -0400

    Funcion para registrar un grandslam

--- app/src/main/java/com/fuzzyapps/tallerprogra/thirdFragment.java
+++ app/src/main/java/com/fuzzyapps/tallerprogra/thirdFragment.java
@@ -2,14 +2,20 @@ package com.fuzzyapps.tallerprogra;
 
 
 import android.app.Fragment;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 
 
 /**
@@ -19,6 +25,9 @@ public class thirdFragment extends Fragment {
     // Inputs
     private EditText year;
     private Button crear;
+
+    //SQLite Variables
+    Connection con;
 
     public thirdFragment() {
         // Required empty public constructor
@@ -41,11 +50,216 @@ public class thirdFragment extends Fragment {
         crear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Variable del año en String
-                String anio = year.getText().toString();
-                Toast.makeText(getActivity(), "Torneo creado "+anio, Toast.LENGTH_SHORT).show();
+                new addTorneoGranBretana().execute();
+                new addTorneoEstadosUnidos().execute();
+                new addTorneoFrancia().execute();
+                new addTorneoAustralia().execute();
+                new addGrandSlam().execute();
             }
         });
     }
+    class addTorneoGranBretana extends AsyncTask<Void, Void, String > {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
 
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            // Split de variables
+            //Variable del año en String
+            //String anio = year.getText().toString();
+            //Toast.makeText(getActivity(), "Torneo creado "+anio, Toast.LENGTH_SHORT).show();
+            String result = "";
+            String driver = "oracle.jdbc.driver.OracleDriver";
+            String UserName = "tallerprogra";
+            String Password = "navia2016 ";
+            String sourceURL = "jdbc:oracle:thin:@200.105.212.50:1521:xe";
+            String cadena = "insert into gfa_torneo(idPais) VALUES('235')";
+            System.out.println(cadena);
+            try{
+                Class.forName(driver).newInstance();
+                con = DriverManager.getConnection(sourceURL,UserName, Password);
+                Statement st = con.createStatement();
+                st.execute(cadena);
+
+                st.close();
+                con.close();
+                result = "ok";
+            }catch (Exception e){
+                Log.e("ERROR", e.toString());
+                result = "";
+            }
+            return result;
+        }
+
+        protected void onPostExecute(String result) {
+            //Toast.makeText(getActivity(), "Ocurrió un problema con el registro.", Toast.LENGTH_SHORT).show();
+        }
+    }
+    class addTorneoEstadosUnidos extends AsyncTask<Void, Void, String > {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            // Split de variables
+            //Variable del año en String
+            //String anio = year.getText().toString();
+            //Toast.makeText(getActivity(), "Torneo creado "+anio, Toast.LENGTH_SHORT).show();
+            String result = "";
+            String driver = "oracle.jdbc.driver.OracleDriver";
+            String UserName = "tallerprogra";
+            String Password = "navia2016 ";
+            String sourceURL = "jdbc:oracle:thin:@200.105.212.50:1521:xe";
+            String cadena = "insert into gfa_torneo(idPais) VALUES('236')";
+            System.out.println(cadena);
+            try{
+                Class.forName(driver).newInstance();
+                con = DriverManager.getConnection(sourceURL,UserName, Password);
+                Statement st = con.createStatement();
+                st.execute(cadena);
+
+                st.close();
+                con.close();
+                result = "ok";
+            }catch (Exception e){
+                Log.e("ERROR", e.toString());
+                result = "";
+            }
+            return result;
+        }
+
+        protected void onPostExecute(String result) {
+            //Toast.makeText(getActivity(), "Ocurrió un problema con el registro.", Toast.LENGTH_SHORT).show();
+        }
+    }
+    class addTorneoFrancia extends AsyncTask<Void, Void, String > {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            // Split de variables
+            //Variable del año en String
+            //String anio = year.getText().toString();
+            //Toast.makeText(getActivity(), "Torneo creado "+anio, Toast.LENGTH_SHORT).show();
+            String result = "";
+            String driver = "oracle.jdbc.driver.OracleDriver";
+            String UserName = "tallerprogra";
+            String Password = "navia2016 ";
+            String sourceURL = "jdbc:oracle:thin:@200.105.212.50:1521:xe";
+            String cadena = "insert into gfa_torneo(idPais) VALUES('78')";
+            System.out.println(cadena);
+            try{
+                Class.forName(driver).newInstance();
+                con = DriverManager.getConnection(sourceURL,UserName, Password);
+                Statement st = con.createStatement();
+                st.execute(cadena);
+
+                st.close();
+                con.close();
+                result = "ok";
+            }catch (Exception e){
+                Log.e("ERROR", e.toString());
+                result = "";
+            }
+            return result;
+        }
+
+        protected void onPostExecute(String result) {
+            //Toast.makeText(getActivity(), "Ocurrió un problema con el registro.", Toast.LENGTH_SHORT).show();
+        }
+    }
+    class addTorneoAustralia extends AsyncTask<Void, Void, String > {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            // Split de variables
+            //Variable del año en String
+            //String anio = year.getText().toString();
+            //Toast.makeText(getActivity(), "Torneo creado "+anio, Toast.LENGTH_SHORT).show();
+            String result = "";
+            String driver = "oracle.jdbc.driver.OracleDriver";
+            String UserName = "tallerprogra";
+            String Password = "navia2016 ";
+            String sourceURL = "jdbc:oracle:thin:@200.105.212.50:1521:xe";
+            String cadena = "insert into gfa_torneo(idPais) VALUES('12')";
+            System.out.println(cadena);
+            try{
+                Class.forName(driver).newInstance();
+                con = DriverManager.getConnection(sourceURL,UserName, Password);
+                Statement st = con.createStatement();
+                st.execute(cadena);
+
+                st.close();
+                con.close();
+                result = "ok";
+            }catch (Exception e){
+                Log.e("ERROR", e.toString());
+                result = "";
+            }
+            return result;
+        }
+
+        protected void onPostExecute(String result) {
+            //Toast.makeText(getActivity(), "Ocurrió un problema con el registro.", Toast.LENGTH_SHORT).show();
+        }
+    }
+    class addGrandSlam extends AsyncTask<Void, Void, String > {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            // Split de variables
+            //Variable del año en String
+            String anio = year.getText().toString();
+            //Toast.makeText(getActivity(), "Torneo creado "+anio, Toast.LENGTH_SHORT).show();
+            String result = "";
+            String driver = "oracle.jdbc.driver.OracleDriver";
+            String UserName = "tallerprogra";
+            String Password = "navia2016 ";
+            String sourceURL = "jdbc:oracle:thin:@200.105.212.50:1521:xe";
+            String cadena = "insert into gfa_grandSlam(anio, idtorneo1, idtorneo2, idtorneo3, idtorneo4) VALUES('"+anio+"', '1', '2', '3','4')";
+            System.out.println(cadena);
+            try{
+                Class.forName(driver).newInstance();
+                con = DriverManager.getConnection(sourceURL,UserName, Password);
+                Statement st = con.createStatement();
+                st.execute(cadena);
+
+                st.close();
+                con.close();
+                result = "ok";
+            }catch (Exception e){
+                Log.e("ERROR", e.toString());
+                result = "";
+            }
+            return result;
+        }
+
+        protected void onPostExecute(String result) {
+            if(result.equals("ok")){
+                Toast.makeText(getActivity(), "Se creó corectamente el grand Slam.", Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(getActivity(), "Ocurrió un problema con el registro.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 }
